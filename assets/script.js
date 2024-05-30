e7ab625bb94685801dad6c08f910fcfec8064559
Alvin Reihansyah <reihansyah6@gmail.com>
2024-05-31 02:25:58 +0700

    done editing splash, index.html, and guide.html. Please edit cam.html and change the .mind.

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,13 @@
 const splashScreen = document.getElementById('splashscreen');
 const splashscreenlogo = document.getElementById('splashscreen-logo');
-if (!sessionStorage.getItem('splashShown')) {
-  function splashOnLoad() {
-    setTimeout(function () {
-      splashscreenlogo.classList.add('show');
-    }, 500);
-
-    setTimeout(function () {
-      splashScreen.classList.add('hidden');
-    }, 3000);
-    setTimeout(function () {
-      splashScreen.remove();
-      sessionStorage.setItem('splashShown', 'true');
-    }, 3800);
-  }
-} else {
-  // Hide splash screen if visited
-  splashScreen.style.display = 'none';
+function splashOnLoad() {
+  setTimeout(function () {
+    splashscreenlogo.classList.add('show');
+  }, 100);
+  setTimeout(function () {
+    splashScreen.classList.add('hidden');
+  }, 2000);
+  setTimeout(function () {
+    splashScreen.remove();
+  }, 2800);
 }
